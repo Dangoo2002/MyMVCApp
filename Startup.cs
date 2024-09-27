f38c6862ac6eaf9ce4ce62885df198f14c212acc
Dangoo2002 <kennydangote893@gmail.com>
2024-09-27 22:29:16 +0300

    hfdf

---
+++ Startup.cs
@@ -0,0 +1,26 @@
+public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();  // This is required to serve CSS and JS files from wwwroot
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+    });
+}
