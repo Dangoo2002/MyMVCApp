dd614d8c26294d41bb504511724dfbdc5c3e23e4
Dangoo2002 <kennydangote893@gmail.com>
2024-09-27 16:09:21 +0300

    yoooo

---
+++ CONTROLLERS/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace YourNamespace.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Index page loaded.");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            _logger.LogInformation("About page loaded.");
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            _logger.LogInformation("Contact page loaded.");
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
